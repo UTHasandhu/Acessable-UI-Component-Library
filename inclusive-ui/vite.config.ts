474eafe3f36839a0be5b89d288f228cd1901b02b
UTHasandhu <hasandhu@utexas.edu>
2025-08-19 15:14:34 -0500

    Setup CI/CD

--- inclusive-ui/vite.config.ts
+++ inclusive-ui/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+import { defineConfig } from "vitest/config";
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -7,5 +8,11 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
-  ]
+  ],
+  // test: {
+  //   globals: true,
+  //   environment: "jsdom",
+  //   setupFiles: "./tests/setupTests.ts",
+  // },
+  base: "inclusive-ui-component-library",
 })
